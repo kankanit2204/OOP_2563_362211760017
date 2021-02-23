088d7278c619b206ede06215c6368ac8fbb5db10
kankankanit <kankanit220400@gmail.com>
2021-02-23 11:53:25 +0700

    BasicInput

---
+++ src/LAB8/Toyota.java
@@ -0,0 +1,28 @@
+package LAB8;
+
+public class Toyota implements ICar {
+    @Override
+    public void engineStart() {
+
+    }
+
+    @Override
+    public void gearChange(int newGear) {
+
+    }
+
+    @Override
+    public void turnLeft() {
+
+    }
+
+    @Override
+    public void turnRight() {
+
+    }
+
+    @Override
+    public void engineBreak() {
+
+    }
+}
